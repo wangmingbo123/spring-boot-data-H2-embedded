802e77a3fcc4a9ad96b8797fd20cc737121ffc52
wangyu106 <wangyu106@jd.com>
2019-06-14 00:02:10 +0800

    l

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,5 +1,5 @@
-CREATE TABLE EMPLOYEE
-        (
-        id NUMBER(10) NOT NULL,
-        name VARCHAR2(50) NOT NULL,
-        );
+CREATE TABLE if not exists EMPLOYEE
+(
+  id   NUMBER(10)   NOT NULL,
+  name VARCHAR2(50) NOT NULL,
+);
